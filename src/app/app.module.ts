cbd95f0df55d8f5aca1ab937b04363423a430a95
devLucasOAK <devlucasoak@outook.com>
2021-10-15 01:36:03 -0300

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonService } from './service/pokemon.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PokemonListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PokemonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+
+  apiUrl: "https://pokeapi.co/api/v2/pokemon/",
   production: false
 };
 
